47274c46241b359a4a74ab86fdab5362a5b47be9
johnsenultra <johnsenultra446@gmal.com>
2025-07-07 00:05:51 +0800

    import the module of @fontsource-variable

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import "./styles/tailwind.css"
-import "@fontsource/poppins"
-import "@fontsource/poppins/200.css"
+import "@fontsource-variable/montserrat"
 import App from "./App.jsx"
 
 createRoot(document.getElementById("root")).render(
