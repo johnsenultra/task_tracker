906ab757064e04ba966dff8e7fc13a785f3f6873
johnsenultra <johnsenultra446@gmail.com>
2025-07-15 01:47:18 +0800

    completed all the input fields

--- src/pages/auth/Signin.jsx
+++ src/pages/auth/Signin.jsx
@@ -12,7 +12,7 @@ export default function Signin() {
             <p>-Peter Drucker</p>
           </div>
         </div>
-        <div className="justify-cente flex w-full items-center p-8 lg:w-1/2">
+        <div className="flex w-full items-center justify-center p-8 lg:w-1/2">
           <div className="w-full max-w-md">
             <div className="mb-8 text-center">
               <h2 className="text-3xl font-bold text-gray-900">
@@ -22,11 +22,12 @@ export default function Signin() {
                 Join Taskly to to make your progress
               </p>
             </div>
+            {/* Input fields */}
             <form className="space-y-6">
               <div className="grid grid-cols-2 gap-4">
                 <div>
                   <label
-                    for="firstName"
+                    htmlFor="firstName"
                     className="block text-sm font-medium text-gray-700"
                   >
                     First Name
@@ -34,11 +35,56 @@ export default function Signin() {
                   <input
                     id="firstName"
                     type="text"
-                    placeholder="First Name"
+                    placeholder="John"
                     required
-                    className="mt-1 block rounded-xl border border-gray-300 px-4 py-3 shadow-sm focus:border-black focus:ring-black focus:outline-none"
+                    className="mt-1 block rounded-xl border border-gray-300 px-3 py-3 shadow-sm focus:border-black focus:ring-black focus:outline-none"
                   />
                 </div>
+                <div>
+                  <label
+                    htmlFor="lastName"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Last Name
+                  </label>
+                  <input
+                    id="lastName"
+                    type="text"
+                    placeholder="Doe"
+                    required
+                    className="mt-1 block rounded-xl border border-gray-300 px-3 py-3 shadow-sm focus:border-black focus:ring-black focus:outline-none"
+                  />
+                </div>
+              </div>
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Email Address
+                </label>
+                <input
+                  id="email"
+                  type="email"
+                  placeholder="johndoe@example.com"
+                  required
+                  className="block w-full rounded-xl border border-gray-300 px-4 py-3 shadow-sm focus:border-black focus:ring-black focus:outline-none"
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block font-medium text-gray-700"
+                >
+                  Password
+                </label>
+                <input
+                  id="password"
+                  type="password"
+                  placeholder="•••••••••"
+                  required
+                  className="mt-1 block w-full rounded-xl border border-gray-300 px-4 py-3 shadow-sm focus:border-black focus:ring-black focus:outline-none"
+                />
               </div>
             </form>
           </div>
