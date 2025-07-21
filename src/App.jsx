a2b0314f7b4a6cc392d07413b9f5cb7eb357ecb1
johnsenultra <johnsenultra446@gmail.com>
2025-07-22 03:06:55 +0800

    Add missing import for Signup component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import LandingPage from "./pages/LandingPage.jsx"
 import Header from "./components/Header.jsx"
 import Signin from "./pages/auth/Signin.jsx"
+import Singup from "./pages/auth/Signup.jsx"
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route path="/" element={<Header />}>
             <Route index element={<LandingPage />} />
             <Route path="signin" element={<Signin />} />
+            <Route path="signup" element={<Singup />} />
           </Route>
         </Routes>
       </BrowserRouter>
